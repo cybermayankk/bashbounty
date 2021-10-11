3ef28c20fd333585d84c72a2e5a94b445a7321f1
Mayank Gandhi <86929443+cybermayankk@users.noreply.github.com>
2021-10-11 05:08:01 -0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,11 +1,11 @@
 git clone https://github.com/cybermayankk/bashbounty.git
 
-chown u+x code.sh
+chmod u+x code.sh     
 
-./bashbounty.sh -d ( enter your target here ) 
+./code.sh -d ( enter your target here ) 
 
 like this 
 
-./bashbounty.sh -d google.com 
+./code.sh -d google.com 
 
 The process will automatically works 
