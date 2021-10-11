0aa2b37131c2992de9b978cba84179e6282f8300
Mayank Gandhi <86929443+cybermayankk@users.noreply.github.com>
2021-10-11 05:00:00 -0700

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,9 @@
+git clone https://github.com/cybermayankk/bashbounty.git
+
+./tool2.sh -d ( enter your target here ) 
+
+like this 
+
+./tool2.sh -d google.com 
+
+The process will automatically works 
